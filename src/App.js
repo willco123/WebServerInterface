9f6b243fab67b5ada2450414147bd4cfa521252f
willco213 <87068102+willco123@users.noreply.github.com>
2022-02-20 21:33:38 +0000

    first commit for interface

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {Route, Redirect, Routes, Router, BrowserRouter, Switch} from 'react-router-dom';
+import Homepage from './components/homepage';
+import Feed from './components/feed';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className = "App-main">
+      <Routes>
+        <Route path="/" element = {<Homepage/>} />
+        <Route path="/feed" element = {<Feed/>} />
+      </Routes>
+    </main>
   );
 }
 
 export default App;
+
---
+++ src/components/feed.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Feed = () => {
+  return(
+    <h1>This is the the feed page</h1>
+)
+};
+
+export default Feed;
+
+//Feed will display a message and sub-messages, with account names listed next to it
+//Admin can view database entries for users and edit account information
